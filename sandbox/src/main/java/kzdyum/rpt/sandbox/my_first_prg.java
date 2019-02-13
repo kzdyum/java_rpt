2b680036e727e5b328a82cacb46bb6da1fa00f70
kzdyum <46928036+kzdyum@users.noreply.github.com>
2019-02-13 12:26:32 +0200

    добавлено несколько функций

--- sandbox/src/main/java/kzdyum/rpt/sandbox/my_first_prg.java
+++ sandbox/src/main/java/kzdyum/rpt/sandbox/my_first_prg.java
@@ -3,8 +3,24 @@ package kzdyum.rpt.sandbox;
 public class my_first_prg {
 
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		hello("Kate");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной "+l+" = "+area(l));
+
+		double a=5, b=2;
+		System.out.println("Площадь прямоугольника со сторонами "+a+" и "+b+" = "+area(a,b));
 
 }
-
+public static void hello(String somebody){
+		System.out.println("Hello, "+somebody+"!");
+}
+public static Double area (double len){
+		return len*len;
+	}
+public static Double area (double a, double b){
+	return	a*b;
+}
 }
