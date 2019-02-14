d4ba283782364e5b1e2cc61222b619aa8c13a123
kzdyum <46928036+kzdyum@users.noreply.github.com>
2019-02-15 00:54:51 +0200

    metod instead of func

--- homework1/src/main/java/Homework1j.java
+++ homework1/src/main/java/Homework1j.java
@@ -1,21 +1,15 @@
 public class Homework1j {
   public static void main (String [] args) {
+    Point pn1=new Point(2.0,5.0);
+    Point pn2=new Point (10.0,12.0);
 
-    Point p1= new Point (2,3);
-    System.out.println("Координаты точки " + "p1: " + p1.x + " ; " + p1.y);
+    System.out.println("Координаты точки " + "p1: " + pn1.x + " ; " + pn1.y);
 
-    Point p2= new Point (4,5);
-    System.out.println("Координаты точки " + "p2: " + p2.x + " ; " + p2.y);
+    System.out.println("Координаты точки " + "p2: " + pn2.x + " ; " + pn2.y);
 
-    System.out.println("Расстояние между точками "+distance(p1,p2));
-
-  }
-
-   public static Point p1=new Point (2.0,3.0);
-   public static Point p2=new Point (4.0,5.0);
-    public static double distance(Point p1,Point p2){
-  return Math.sqrt(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
+    System.out.println("Расстояние между точками "+ pn2.distance(pn1));
 
   }
 
-}
+  }
+
--- homework1/src/main/java/Point.java
+++ homework1/src/main/java/Point.java
@@ -1,9 +1,21 @@
 public class Point {
- public double x;
- public double y;
+  public double x;
+  public double y;
 
-public Point (double x, double y){
-  this.x=x;
-  this.y=y;
-}
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  //Point pt1 = new Point(x, y);
+  //Point pt2 = new Point(x, y);
+
+
+  public double distance(double x, double y) {
+
+    return Math.sqrt(((this.x-x) * (this.x-x)) + ((this.y-y) * (this.y-y)));
+  }
+  public double distance (Point p){
+    return distance (p.x,p.y);
+  }
 }
