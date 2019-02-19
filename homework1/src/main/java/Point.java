486dda156f8e326d54231aa14d7d781463155cf2
kzdyum <46928036+kzdyum@users.noreply.github.com>
2019-02-19 13:55:50 +0200

    code formatting

--- homework1/src/main/java/Point.java
+++ homework1/src/main/java/Point.java
@@ -6,10 +6,8 @@ public class Point {
     x = a;
     y = b;
   }
-
   public double distance (Point p){
     return Math.sqrt(((p.x-x) * (p.x-x)) + ((p.y-y) * (p.y-y)));
-
   }
   public double distanceFormat (Point p){
     return Math.round(distance(p)*100d)/100d;
