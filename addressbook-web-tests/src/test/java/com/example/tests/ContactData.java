9bc23421154e9c75d5b4838d5133b952aa27b21b
kzdyum <46928036+kzdyum@users.noreply.github.com>
2019-02-27 12:40:42 +0200

    final update of ContactCreationTest test

---
+++ addressbook-web-tests/src/test/java/com/example/tests/ContactData.java
@@ -0,0 +1,43 @@
+package com.example.tests;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String address;
+  private final String mobilephone;
+  private final String email;
+
+  public ContactData(String firstname, String middlename, String lastname, String address, String mobilephone, String email) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.address = address;
+    this.mobilephone = mobilephone;
+    this.email = email;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
--- addressbook-web-tests/src/test/java/kzdyum/rpt/addressbook/TestContactCreation.java
+++ addressbook-web-tests/src/test/java/kzdyum/rpt/addressbook/TestContactCreation.java
@@ -1,12 +1,14 @@
 package com.example.tests;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
 
 public class TestContactCreation {
   private WebDriver driver;
@@ -19,35 +21,57 @@ public class TestContactCreation {
     driver = new FirefoxDriver();
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.xpath("//input[@value='LOGIN']")).click();
   }
 
   @Test
   public void testContactCreation() throws Exception {
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value='LOGIN']")).click();
-    driver.findElement(By.linkText("ADD_NEW")).click();
+    initContactCreation();
+    fillContactForm(new ContactData(
+            "Ekaterina",
+            "A",
+            "Zdyumayeva",
+            "202a, Minusinska str., Dnipro, Ukraine, 49000",
+            "0667519212",
+            "kzdyum@gmail.com"));
+    submitContactForm();
+  }
+
+  private void submitContactForm() {
+    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillContactForm(ContactData contactData) {
     driver.findElement(By.name("firstname")).click();
     driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys("Ekaterina");
+    driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
     driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys("A");
+    driver.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
     driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys("Zdyumayeva");
+    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     driver.findElement(By.name("address")).click();
     driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys("202a, Minusinska str., Dnipro, Ukraine, 49000");
+    driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
     driver.findElement(By.name("mobile")).click();
     driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys("0667519212");
+    driver.findElement(By.name("mobile")).sendKeys(contactData.getMobilephone());
     driver.findElement(By.name("email")).click();
     driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("kzdyum@gmail.com");
-    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
+  }
+
+  private void initContactCreation() {
+    driver.findElement(By.linkText("ADD_NEW")).click();
   }
 
   @AfterClass(alwaysRun = true)
